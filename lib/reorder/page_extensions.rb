dcaa7adc5e65235c1378d93f7a0ca7890e85c4f4
Kunal Shah <kunal@digitalpulp.com>
2009-08-04 16:19:56 -0400

    Make Reorder work with Copy/Move out of the box

--- lib/reorder/page_extensions.rb
+++ lib/reorder/page_extensions.rb
@@ -3,10 +3,19 @@ module Reorder::PageExtensions
     base.class_eval {
       acts_as_list :scope => :parent_id
       self.reflections[:children].options[:order] = "position ASC"
+      before_update :check_position
     }
     
     if defined?(Page::NONDRAFT_FIELDS)
       Page::NONDRAFT_FIELDS << 'position'
     end
+    
+    private
+      # Make sure Reorder works with CopyMove
+      def check_position
+        return unless parent_id_changed? && parent
+        self.position = Page.find(parent_id).children.size+1
+      end
+    
   end
 end
