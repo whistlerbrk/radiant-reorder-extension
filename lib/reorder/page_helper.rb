7f42a6cd8b93cc30ef96540984efee934548a563
Kunal Shah <kunal@digitalpulp.com>
2009-08-04 16:19:24 -0400

    Update order links to match new routing

--- lib/reorder/page_helper.rb
+++ lib/reorder/page_helper.rb
@@ -1,11 +1,10 @@
 module Reorder::PageHelper
   def order_links(page)
     returning String.new do |output|
-      %w{move_to_top move_higher move_lower move_to_bottom}.each do |action|
-        output << link_to(image("#{action}.png", :alt => action.humanize), 
-                          self.send("page_#{action}_url", :id => page), 
-                          :method => :post)
-      end
+      output << link_to(image("move_to_top.png", :alt => "Move to top"), move_to_top_admin_page_path(page), :method => :put)
+      output << link_to(image("move_higher.png", :alt => "Move up"), move_higher_admin_page_path(page), :method => :post)
+      output << link_to(image("move_lower.png", :alt => "Move down"), move_lower_admin_page_path(page), :method => :post)
+      output << link_to(image("move_to_bottom.png", :alt => "Move to bottom"), move_to_bottom_admin_page_path(page), :method => :put)
     end
   end
 end
